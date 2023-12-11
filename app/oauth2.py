bbd7b388e35661700cf94bec566127f807a5ad0b
adhishrayas <adhishrayas@gmail.com>
2023-12-11 23:15:36 +0530

    added stuff

--- app/oauth2.py
+++ app/oauth2.py
@@ -43,7 +43,7 @@ def require_user(Authorize: AuthJWT = Depends()):
     except Exception as e:
         error = e.__class__.__name__
         print(error)
-        if error == 'MissingTokenError':
+        if error == 'MissingTokenError' :
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED, detail='You are not logged in')
         if error == 'UserNotFound':
